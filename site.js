d92cc8fa421bcbb11c96e2e9fc57933c87b58013
Kem <treekems@gmail.com>
2019-02-10 17:59:11 -0600

    Code added to site.js, outline code that detects key input into the form. Disabled sign up button unless the character length is greater than 5. Added regular expression to validate email input

--- site.js
+++ site.js
@@ -1,3 +1,24 @@
+var submit_form = document.querySelector('#signup');
+var info_input1 = document.querySelector('#input');
+
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOM Content Loaded');
+  document.getElementById('signup').disabled =true;
 })
+
+info_input1.addEventListener('keyup', function(){
+  var x = this.value.length;
+  if (x > 5) {
+      document.getElementById('signup').disabled = false;
+    }
+})
+
+function vtest(info_input1){
+  var email_valid = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+}
+
+function valid() {
+  var output = ('');
+
+}
