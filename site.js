84d792c3733ba9641abc545d56dcf75db8da3f49
Kem <treekems@gmail.com>
2019-02-10 19:14:57 -0600

    Added more js code for email validation and submitting. Changed index html to change the id and text content of the label tag.

--- site.js
+++ site.js
@@ -13,12 +13,21 @@ info_input1.addEventListener('keyup', function(){
     }
 })
 
-function vtest(info_input1){
+var input1;
+var check;
+submit_form.addEventListener('click', function() {
+  input1 = document.getElementById('input').value;
+  console.log(input1);
   var email_valid = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  check = email_valid.test(input1);
+  console.log(input1);
+  if(check === true) {
+    document.getElementById('ltext').innerText = 'Email: Valid!';
+    document.getElementById('ltext').style.color = 'green';
+    }
+    else {
+    document.getElementById('ltext').innerText = 'Email: Invalid!';
+    document.getElementById('ltext').style.color = 'red';
+    }
 
-}
-
-function valid() {
-  var output = ('');
-
-}
+})
